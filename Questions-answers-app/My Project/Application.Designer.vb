3b7fd6bd0b1aab0da49653e66c410977c517b3d7
Wijong Kim <wxk001@email.uark.edu>
2017-10-19 08:33:38 -0500

    rename welcome form

--- Questions-answers-app/My Project/Application.Designer.vb
+++ Questions-answers-app/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Questions_answers_app.Form1
+            Me.MainForm = Global.Questions_answers_app.welcome
         End Sub
     End Class
 End Namespace
